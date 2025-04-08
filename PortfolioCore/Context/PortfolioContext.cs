77b949d7e0fc171d8ea402788de855dcc8cd91e9
aysemerveyildiz <meerve.ylz@gmail.com>
2025-04-08 22:05:11 +0300

    Add project files.

---
+++ PortfolioCore/Context/PortfolioContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioCore.Context
+{
+    public class PortfolioContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MERMILIUS;initial Catalog=PortfolioNightDb;integrated security=true;");
+        }
+        public DbSet<Entities.Contact> Contacts { get; set; }
+        public DbSet<Entities.Education> Educations { get; set; }
+        public DbSet<Entities.Experience> Experiences { get; set; }
+        public DbSet<Entities.Message> Messages { get; set; }
+        public DbSet<Entities.Testimonial> Testimonials { get; set; }
+        
+    }
+}
